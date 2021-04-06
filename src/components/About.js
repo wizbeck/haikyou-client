9ae950e66159bdd22bca96b31298b1df000a1e57
Wesley <wes.beck21@outlook.com>
2021-04-06 12:46:59 -0700

    about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 class About extends Component {
   render() {
     return (
       <div>
-        About page
+        <h1>About this website</h1>
+        <p> Haikyou is a little webapp thought up to express the long lost art of haikus and their simple yet elegant nature of expression through a few lines and syllables.</p>
+        <p> Please don't hesitate to share an expression of your self, or something on your mind in a beautiful wording on Haikyou!</p>
+        <p>Click <Link to="/new">here</Link> to share or create a haiku on Haikyou!</p>
       </div>
     )
   }
