d0446a0d8095c0c10bf81632322279f610dd77aa
Wesley <wes.beck21@outlook.com>
2021-04-08 17:26:16 -0700

    quotes wrap poems

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -7,9 +7,9 @@ class Poem extends Component {
     return (
       <div className="poem">
         <h3>{ title }</h3>
-        <p>{ line_1 }</p>
+        <p>{'"'}{ line_1 }</p>
         <p>{ line_2 }</p>
-        <p>{ line_3 }</p>
+        <p>{ line_3 }{'"'}</p>
         <span> - { author }</span>
         {/* add likes later */}
       </div>
