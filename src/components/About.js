5f4fc4bb9369d4036718224cd2fd6cb85057f54e
Wesley <wes.beck21@outlook.com>
2021-04-10 16:45:39 -0700

    fixed link component

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ class About extends Component {
         <h1>About this website</h1>
         <p> Haikyou is a little webapp thought up to express the long lost art of haikus and their simple yet elegant nature of expression through a few lines and syllables.</p>
         <p> Please don't hesitate to share an expression of your self, or something on your mind in a beautiful wording on Haikyou!</p>
-        <p>Click <Link to="/new">here</Link> to share or create a haiku on Haikyou!</p>
+        <p>Click <Link to="/haikus/new">here</Link> to share or create a haiku on Haikyou!</p>
       </div>
     )
   }
