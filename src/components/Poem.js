83b19f30c8ea84ab052845f284de7e97e2f412c2
Wesley <wes.beck21@outlook.com>
2021-04-15 10:15:46 -0700

    like button not functional

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
-
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
 class Poem extends Component {
+
+  handleClick = (id) => {
+    // add a like to total likes
+    // dispatch action fetch request post to `http://localhost:3000/${id}`
+    // update state of individual poem when clicking
+  }
+
   render() {
-    const {title, line_1, line_2, line_3, author } = this.props
+    const {id, title, line_1, line_2, line_3, author, likes} = this.props
 
     return (
       <div className="poem">
@@ -11,7 +19,8 @@ class Poem extends Component {
         <p>{ line_2 }</p>
         <p>{ line_3 }{'"'}</p>
         <span> - <em>{ author }</em></span>
-        {/* add likes later */}
+        <br />
+        <span className="poem__likes"><ThumbUpAltIcon onClick={this.handleClick(id)}/> {likes} </span>
       </div>
     )
   }
